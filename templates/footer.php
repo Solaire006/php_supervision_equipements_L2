c60e51ecdb6231d114422db9d4d68cedbd7e75c7
BI-N4 <randrianjakafihobiana@gmail.com>
2026-03-29 19:07:57 +0300

    liaison du code PHP avec le fichier JS

--- templates/footer.php
+++ templates/footer.php
@@ -2,5 +2,6 @@
     <footer>
         <p>&copy; 2026 Supervision des Équipements.</p>
     </footer>
+    <script src="/php_supervision_equipements_L2/public/assets/js/scripts.js"></script>
 </body>
 </html>
